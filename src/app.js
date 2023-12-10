7e772a263675ef43f5a9545ca5d213c4ccdccee4
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2023-12-11 03:17:22 +0900

    refactor: 심화주차 강의대로 일단 변경

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
+import router from './routes/index.js';
+import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
 const app = express();
-const PORT = 3018;
+const port = 4000;
 
+app.use(LogMiddleware);
 app.use(express.json());
-app.use(cookieParser());
-app.use('/api', []);
+app.use('/api', router);
+app.use(ErrorHandlingMiddleware);
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-undef
-  console.log(PORT, '포트로 서버가 열렸어요!');
+app.listen(port, () => {
+  console.log(port, '포트로 서버가 열렸어요!');
 });
