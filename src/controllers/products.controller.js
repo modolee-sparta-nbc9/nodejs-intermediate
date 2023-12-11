7c737ba796845c8e04cddab8eb85574ae7aa8289
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2023-12-11 11:04:14 +0900

    refactor: products 라우터, 컨트롤러 연결, 서비스 연결 준비

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,32 +1,36 @@
-// src/controllers/Products.controller.js 심화 참고해서 만들기
+// 심화 참고해서 만들기
 
+//서비스계층에서 가져오기
 import { ProductsService } from '../services/products.service.js';
 
 // Products의 컨트롤러(Controller)역할을 하는 클래스
 export class ProductsController {
-  productsService = new ProductsService(); // Products 서비스를 클래스를 컨트롤러 클래스의 멤버 변수로 할당합니다.
+  // 소문자 프로덕츠서비스 : 대문자 프로덕츠컨드롤러 클래스에 멤버 변수로 할당
+  productsService = new ProductsService();
 
+  /** 상품 조회 API */
   getProducts = async (req, res, next) => {
     try {
-      // 서비스 계층에 구현된 findAllProducts 로직을 실행합니다.
+      // 서비스 계층 findAllProducts 메서드 사용하기
       const products = await this.productsService.findAllProducts();
 
-      return res.status(200).json({ data: products });
+      return res.status(200).json({ data: products }); // 서비스에서 가져온 것 리스폰스로 할당
     } catch (err) {
       next(err);
-    }
+    } //에러 전역 미들웨어로 에러 전달
   };
 
+  /** 상품 생성 API */
   createProduct = async (req, res, next) => {
     try {
-      const { nickname, password, title, content } = req.body;
+      const { userId, title, description, status } = req.body;
 
-      // 서비스 계층에 구현된 createProducts 로직을 실행합니다.
+      // 서비스 계층 createProduct 메서드 사용
       const createdProduct = await this.productsService.createProduct(
-        nickname,
-        password,
+        userId,
         title,
-        content,
+        description,
+        status,
       );
 
       return res.status(201).json({ data: createdProduct });
