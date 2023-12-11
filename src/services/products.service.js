98bae0cdbd6ede89c5b7c0946d247377f395f23e
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2023-12-11 12:02:30 +0900

    refactor: products 서비스 주석 수정

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -9,6 +9,7 @@ export class ProductsService {
   findAllProducts = async () => {
     // 저장소(Repository)에게 데이터를 요청
     const products = await this.productsRepository.findAllProducts();
+    //레포가 어떻게 구성 되든, 여기서는 아무튼 파인드 올 프로덕츠 보고 어쨌든 가져와서 사용하는 구나 인지하면 된다.
 
     // 호출한 프로덕츠 가장 최신 게시글 부터 정렬합니다.
     products.sort((a, b) => {
@@ -16,6 +17,7 @@ export class ProductsService {
     });
 
     // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공
+    // 해당하는 특정 데이터를 빼서 파싱하는 작업도 수행
     return products.map((product) => {
       return {
         productId: product.productId,
@@ -40,6 +42,7 @@ export class ProductsService {
     );
 
     // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공
+    // 중요한 정보 외부로 노출시키지 않도록 서비스 계층에서 코드를 작성
     return {
       productId: createdProduct.productId,
       userId: createdProduct.userId,
